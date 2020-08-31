5205826fd531c022316da815b04963f19873d724
Kishor Kumar <kishorgn@gmail.com>
2020-08-31 10:12:57 +0530

    Version 1.1, with 2 lines of code.

--- ArrayDemo_01/src/com/dxc/demo/Demo_01.java
+++ ArrayDemo_01/src/com/dxc/demo/Demo_01.java
@@ -5,6 +5,7 @@ public class Demo_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a simple program (No array)");
+		System.out.println("This is second line");
 
 	}
 
