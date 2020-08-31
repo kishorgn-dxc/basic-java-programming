014ee1c831913193dc8418cab33c992cb50bd536
Kishor Kumar <kishorgn@gmail.com>
2020-08-31 10:11:07 +0530

    Project Version - 1.0, with only program Demo_01 with 1 line of code

---
+++ ArrayDemo_01/src/com/dxc/demo/Demo_01.java
@@ -0,0 +1,11 @@
+package com.dxc.demo;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a simple program (No array)");
+
+	}
+
+}
